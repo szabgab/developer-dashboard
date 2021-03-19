9aef5d28d6b3bb950425a57ab33da0f602321dfd
Gabor Szabo <gabor@szabgab.com>
2021-03-19 07:47:47 +0200

    show user

--- app.pl
+++ app.pl
@@ -10,6 +10,14 @@ get '/' => sub ($c) {
     $c->render(template => 'users', users => $users);
 };
 
+get '/github/:name' => sub ($c) {
+    my $name = $c->stash('name');
+
+    my $users = decode_json(path('data/users.json')->slurp_utf8);
+    $c->render(template => 'user', user => $users->{$name});
+};
+
+
 app->start;
 __DATA__
 
@@ -22,9 +30,16 @@ __DATA__
 <h1>Users</h1>
 <% for my $user (sort keys %{ $users }) { %>
    <hr>
-   <a href="https://github.com/<%= $user %>">
+   <a href="/github/<%= $user %>">
        <img class="avatar" src="<%= $users->{$user}{avatar_url} %>">
        <%= $users->{$user}{name} %>
    </a>
 <% } %>
+
+@@ user.html.ep
+
+   <a href="https://github.com/<%= $user %>">
+       <img class="avatar" src="<%= $user->{avatar_url} %>">
+       <%= $user->{name} %>
+   </a>
 
