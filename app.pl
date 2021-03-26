afda26b2cba9ea8963803f7aaff3274b68f24cec
Gabor Szabo <gabor@szabgab.com>
2021-03-26 11:37:52 +0300

    show the open issue count; show the description of each repo

--- app.pl
+++ app.pl
@@ -3,6 +3,7 @@ use Mojolicious::Lite -signatures;
 #plugin 'AutoReload';
 use Path::Tiny qw(path);
 use Mojo::JSON qw(decode_json encode_json);
+use List::Util qw(sum0);
 
 
 get '/' => sub ($c) {
@@ -15,7 +16,13 @@ get '/github/:name' => sub ($c) {
 
     my $users = decode_json(path('data/users.json')->slurp_utf8);
     my $repos = decode_json(path('data/repos.json')->slurp_utf8);
-    $c->render(template => 'user', name => $name, user => $users->{$name}, repos => $repos->{$name});
+    my $open_issues = sum0 map {$_->{open_issues}} values %{ $repos->{$name} };
+    $c->render(template => 'user',
+        name => $name,
+        user => $users->{$name},
+        repos => $repos->{$name},
+        open_issues => $open_issues,
+    );
 };
 
 app->start;
